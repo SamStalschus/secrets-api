af4b2736b5d9f16b8505882d5c57bef3edd99f7d
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-10-24 01:26:07 -0300

    Feat: Add - config request_id of application

---
+++ cmd/secrets-api/middlewares/request_id.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"net/http"
+	"secrets-api/domain"
+	"secrets-api/infra/log"
+)
+
+func HandleRequestID(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestID := domain.GetField(r.Context(), domain.RequestIDKey)
+		if requestID == "" {
+			requestID = domain.GenerateRequestID()
+		}
+
+		ctx := domain.CtxWithValues(r.Context(), log.Body{
+			domain.RequestIDKey: requestID,
+		})
+
+		h.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
---
+++ domain/request_id.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+// RequestIDKey needs to be a string, since its used on Fiber `.Locals()`
+const RequestIDKey string = "request_id"
+
+func GenerateRequestID() string {
+	return uuid.NewString()
+}
+
+func GetRequestIDFromContext(ctx context.Context) string {
+	body, _ := ctx.Value(logCtxKey{}).(ctxBody)
+	if body == nil || body[RequestIDKey] == nil {
+		return ""
+	}
+	return body[RequestIDKey].(string)
+}
