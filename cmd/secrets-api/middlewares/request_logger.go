ef77cefb8c761344ca5f7b13d602bd55ee6f374c
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-10-24 01:27:17 -0300

    Feat: Add - middleware of logs and logs client

---
+++ cmd/secrets-api/middlewares/request_logger.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"io/ioutil"
+	"net/http"
+	"secrets-api/infra/log"
+	"time"
+)
+
+func RequestLogger(h http.HandlerFunc, logger log.Provider) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+
+		h.ServeHTTP(w, r)
+
+		body, _ := ioutil.ReadAll(r.Body)
+
+		logger.Info(r.Context(), "request-completed", log.Body{
+			"route":        r.Method + " " + r.URL.Path,
+			"request_body": body,
+			"duration_ms":  time.Since(startTime).Milliseconds(),
+		})
+	}
+}
