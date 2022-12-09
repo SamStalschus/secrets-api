4dc33890be1aff6da29c9ed0e71a077c465f1e35
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-12-09 18:39:50 -0300

    Feat: Add - CORS configuration

--- cmd/secrets-api/url_mappings.go
+++ cmd/secrets-api/url_mappings.go
@@ -46,8 +46,6 @@ func initializeRoutes() {
 				middlewares.RequestLogger(
 					middlewares.EnsureAuth(
 						secretController.GetSecret, authProvider, logger), logger))),
-
-		//newRoute("PUT", "/secret/([0-9]+)", updateSecret),
 	}
 }
 
@@ -62,10 +60,7 @@ type route struct {
 }
 
 func Server(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "OPTIONS" {
-		w.WriteHeader(http.StatusOK)
-	}
-	//enableCors(&w)
+	enableCors(&w, r)
 	initializeRoutes()
 	var allow []string
 	for _, route := range routes {
@@ -95,12 +90,31 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Pong"))
 }
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
-	(*w).Header().Set("Access-Control-Expose-Headers", "Authorization")
-	(*w).Header().Set("Access-Control-Expose-Credentials", "true")
-	(*w).Header().Set("Access-Control-Max-Age", "30000")
-	(*w).Header().Set("Content-Type", "application/json; charset=UTF-8")
+func Handler(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+			w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+			w.Header().Set("Access-Control-Expose-Credentials", "true")
+			w.Header().Set("Access-Control-Max-Age", "30000")
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		} else {
+			Server(w, r)
+		}
+	}
+}
+
+func enableCors(w *http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		(*w).Header().Set("Access-Control-Allow-Origin", "*")
+		(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		(*w).Header().Set("Access-Control-Expose-Headers", "Authorization")
+		(*w).Header().Set("Access-Control-Expose-Credentials", "true")
+		(*w).Header().Set("Access-Control-Max-Age", "30000")
+		(*w).Header().Set("Content-Type", "application/json; charset=UTF-8")
+		return
+	}
 }
