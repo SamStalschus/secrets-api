ac3d31aba587607477eb78709315b67e6b294a15
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-12-09 18:42:06 -0300

    Feat: Add - CORS configuration

--- cmd/secrets-api/url_mappings.go
+++ cmd/secrets-api/url_mappings.go
@@ -108,6 +108,7 @@ func Handler(h http.HandlerFunc) http.HandlerFunc {
 
 func enableCors(w *http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
+		(*w).WriteHeader(http.StatusOK)
 		(*w).Header().Set("Access-Control-Allow-Origin", "*")
 		(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
