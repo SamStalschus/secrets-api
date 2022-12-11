15e54d5d3a6696f11ebcd19dbc11971761ce7b17
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-12-11 02:35:06 -0300

    Fix: alter config of preflight and cors

--- cmd/secrets-api/url_mappings.go
+++ cmd/secrets-api/url_mappings.go
@@ -60,6 +60,10 @@ type route struct {
 }
 
 func Server(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers:", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
